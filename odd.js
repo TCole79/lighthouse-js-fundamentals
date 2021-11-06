87acb5768567fb228d8823fe2e51110b78b4a35e
Tristan Cole <tristanleighcole@googlemail.com>
2021-11-06 21:47:09 +0000

    Function test to see if # is odd or even

---
+++ odd.js
@@ -0,0 +1,11 @@
+// way below is anonymous function
+// let isOdd = function(num) {
+
+// this way is function declaration
+  function isOdd (num) {
+   return num % 2;
+  }
+
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
